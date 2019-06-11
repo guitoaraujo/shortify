1b2aadff9fdb5518dec2b0d3bc019af4f40fcade
igor araujo <igor.araujo@fulllab.com.br>
2019-06-11 15:47:31 -0300

    refactoring populate_db_worker

--- app/controllers/api/v1/sites_controller.rb
+++ app/controllers/api/v1/sites_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::SitesController < ActionController::Base
     response  = Sites::Shortify.call(site).response
     if response
       FetchTitlesWorker.perform_async(site.id)
+      site.update(visits: site.visits+1)
       render json: { status: 200, shor_url: site.shor_url }
     else
       render json: { status: 204, shor_url: nil }
