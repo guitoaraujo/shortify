09054f1dc8cb70690f2fbd594e49a61523ec5207
igor araujo <igor.araujo@fulllab.com.br>
2019-06-11 00:43:06 -0300

    implemented test for uniq long_url validation

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -2,8 +2,17 @@ require 'rails_helper'
 
 RSpec.describe Site, type: :model do
   context 'validations' do
+    before(:each) do
+      Site.new(long_url: 'http://test.com').save
+    end
+
     it 'long_url must be exist' do
       site = Site.new.save
+      expect(site).to eq(false)
+    end
+
+    it 'long_url must be uniq' do
+      site = Site.new(long_url: 'http://test.com').save
       expect(site).to eq(false)
     end
 
